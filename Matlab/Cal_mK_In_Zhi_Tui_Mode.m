bcf1672e07c5761c94cb63af50de6b332724eb49
ysp-021228 <1136722760@qq.com>
2023-12-13 22:29:42 +0800

    Change the LQR calculation to a discrete system

--- Matlab/Cal_mK_In_Zhi_Tui_Mode.m
+++ Matlab/Cal_mK_In_Zhi_Tui_Mode.m
@@ -5,7 +5,8 @@ function [K] = Cal_mK_In_Zhi_Tui_Mode(paras)
 [A_ballance, B_ballance] = Cal_mA_mB_In_Zhi_Tui_Mode(paras(1));
 %[A_ballance, B_ballance] = Cal_mA_mB_In_Zhi_Tui_Mode(0.18);
 %% 调用lqr函数
-T = 0.001;%仿真步长
+T = 0.005;%底盘任务周期
+[G,H]=c2d(A_ballance,B_ballance,T);
 
 Q = [1.8   0    0    0    0     0;   %腿倾角
      0    2.5   0    0    0     0;   %腿倾角速度
@@ -18,7 +19,8 @@ Q = [1.8   0    0    0    0     0;   %腿倾角
 R = [5      0 ;  %轮子
      0        0.2];   %关节
 
-K = lqr(A_ballance, B_ballance, Q, R);
+K = dlqr(G, H, Q, R);
+%K = lqr(A_ballance, B_ballance, Q, R);
 
 %fprintf('{%f,%f,%f,%f,%f,%f},\n',K(1,1),K(1,2),K(1,3),K(1,4),K(1,5),K(1,6));
 %fprintf('{%f,%f,%f,%f,%f,%f},\n\n',K(2,1),K(2,2),K(2,3),K(2,4),K(2,5),K(2,6));
